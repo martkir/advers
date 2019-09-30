55bd59f756d7e5911435fdb0428ecdfb97dfe47a
martkir <martinbarut@hotmail.com>
2019-09-30 17:15:18 +0100

    modified some of the comments

--- attacks/cutout.py
+++ attacks/cutout.py
@@ -8,6 +8,8 @@ class Cutout(nn.Module):
     Args:
         n_holes (int): Number of patches to cut out of each image.
         length (int): The length (in pixels) of each square patch.
+
+    Note: This code was written to be applied on a
     """
     def __init__(self, n_holes, length):
         super(Cutout, self).__init__()
@@ -19,10 +21,11 @@ class Cutout(nn.Module):
     def forward(self, img):
         """
         Args:
-            img (Tensor): Tensor image of size (C, H, W).
+            img (Tensor): Image of shape (n_channels, height, width).
         Returns:
             Tensor: Image with n_holes of dimension length x length cut out of it.
         """
+
         h = img.size(1)
         w = img.size(2)
 
@@ -37,10 +40,19 @@ class Cutout(nn.Module):
             x1 = np.clip(x - self.length // 2, 0, w)
             x2 = np.clip(x + self.length // 2, 0, w)
 
-            mask[y1: y2, x1: x2] = 0.
+            mask[y1: y2, x1: x2] = 0.  # binary matrix shape (height, width).
 
         mask = torch.from_numpy(mask)
         mask = mask.expand_as(img)
         img = img * mask
 
         return img
+
+    def set_epoch(self, epoch):
+        """ Must be implemented in order to use attack with <trainer.py>.
+        """
+        self.epoch = epoch
+        self._update_params(epoch)
+
+    def _update_params(self, epoch):
+        pass
