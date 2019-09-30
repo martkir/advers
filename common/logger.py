313d4fa062f31234c93417d632f40410c8e34a50
martkir <martinbarut@hotmail.com>
2019-09-30 17:09:28 +0100

    updated logger. now capable of generating plots of statistics during training

--- common/logger.py
+++ common/logger.py
@@ -1,59 +1,79 @@
-import json
 import os
 import torch
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 class Logger(object):
-    def __init__(self, job_type, run_id, log_dir=None, flags=None):
-        self.job_type = job_type
-        self.log_dir = log_dir
+    def __init__(self, log_dir=None, flags=None):
+        self.log_dir = log_dir  # e.g. train/train-20190928_173723-3b129d10
         self.flags = flags
 
-        if job_type == 'eval':
-            self.img_dir = os.path.join(self.log_dir, 'images')
-            if self.img_dir is not None:
-                os.mkdir(self.img_dir)
+        # directory where images are saved:
+        self.img_dir = os.path.join(self.log_dir, 'images')
+        if self.img_dir is not None:
+            os.mkdir(self.img_dir)
+
+        # vals saved epoch results.
         self.vals = {}
-        self.summary = {}
 
-        self.log_summary(flags)
-        if self.job_type == 'train':
-            self.log_summary({'run_id': run_id})
+        # config_dict saves experiment arguments e.g. hyperparameters and training options.
+        self.config_dict = {}
+        self._write_config(flags)
 
-    def log(self, log_dict, step):
+    def add(self, log_dict):  # done.
+        """ Adds data to log_dict but does not write to folder.
+        """
         for k, v in log_dict.items():
             if k not in self.vals:
                 self.vals[k] = []
-            self.vals[k].append((v, step))
+            self.vals[k].append(v)
 
-    def log_image(self, image, filestr):
-        img_file = os.path.join(self.img_dir, filestr)
-        image.save(img_file)
+    def _write_config(self, flags, extension='csv'):  # done.
+        for k, v in flags.items():
+            self.config_dict[k] = v
 
-    def log_summary(self, log_dict):
-        for k, v in log_dict.items():
-            self.summary[k] = v
+        df = pd.DataFrame.from_dict(self.config_dict)
+        if extension == 'json':
+            config_file = os.path.join(self.log_dir, 'config.json')
+            df.to_json(config_file, index=False)
+        else:
+            config_file = os.path.join(self.log_dir, 'config.csv')
+            df.to_csv(config_file, index=False, header=True, sep=',')
 
-    def log_ckpt(self, model, optim):
+    def write_vals(self, extension='csv'):  # done.
+        df = pd.DataFrame.from_dict(self.vals)
+        if extension == 'json':
+            all_file = os.path.join(self.log_dir, 'all.json')
+            df.to_json(all_file, index=False)
+        else:
+            all_file = os.path.join(self.log_dir, 'all.csv')
+            df.to_csv(all_file, index=False, header=True, sep=',')
+
+    def write_plot(self, keys=None, description=None):
+        n_epochs = len(self.vals[list(self.vals.keys())[0]])
+        epochs = [i for i in range(n_epochs)]
+        fig, ax = plt.subplots(1, 1)
+
+        if keys is None:
+            for k, vals in self.vals.items():
+                ax.plot(epochs, vals, linewidth=1, label=k)
+        else:
+            for k in keys:
+                ax.plot(epochs, self.vals[k], linewidth=1, label=k)
+
+        if description:
+            ax.set_xlabel(description)
+
+        ax.legend(loc=2)
+        fig.set_figheight(5.5)
+        fig.set_figwidth(11)
+        plt.savefig(os.path.join(self.img_dir, 'plot.png'))
+
+    def save_ckpt(self, model, optim):
+        """ This is called after the last epoch has finished. A different method is used to save checkpoints after
+        each epoch.
+        """
         state = {'model': model.state_dict(),
                  'optimizer': optim.state_dict()}
-        # automatically uploaded to wandb after the run
         torch.save(state, os.path.join(self.log_dir, 'ckpt.pth'))
-
-    def write(self):
-        all_file = os.path.join(self.log_dir, 'all.log')
-        with open(all_file, 'w') as f:
-            json.dump(self.vals, f)
-
-    def write_summary(self):
-        summary_file = os.path.join(self.log_dir, 'summary.log')
-        with open(summary_file, 'w') as f:
-            json.dump(self.summary, f)
-
-    def end(self, summarize_vals=False):
-        if summarize_vals:
-            summary_dict = {}
-            for k, v in self.vals.items():
-                summary_dict[k] = v[-1][0]
-            self.log_summary(summary_dict)
-        self.write_summary()
