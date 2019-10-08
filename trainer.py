14a159d8d63f1126fbe6f58e237b53f44ea032d7
martkir <martinbarut@hotmail.com>
2019-10-08 19:28:37 +0100

    re-organized and fixed some mistakes

--- trainer.py
+++ trainer.py
@@ -24,6 +24,40 @@ class Metric(object):
         return self.sum / self.n
 
 
+class DatasetAdversCIFAR10(datasets.CIFAR10):
+    def __init__(self, transform_adv=None, **kwargs):
+        super(DatasetAdversCIFAR10, self).__init__(**kwargs)
+        self.transform_adv = transform_adv
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+
+        img, target = self.data[index], self.targets[index]
+
+        # doing this so that it is consistent with all other datasets
+        # to return a PIL Image
+        img = Image.fromarray(img)
+        img_adv = img
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.transform_adv is not None:
+            img_adv = self.transform_adv(img_adv)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        output = (img, img_adv, target)
+        return output
+
+
 def accuracy(output, target):
     # get the index of the max log-probability
     pred = output.max(1, keepdim=True)[1]
@@ -37,19 +71,18 @@ def correct(output, target):
 
 
 class BaseTrainer(object):
-    def __init__(self, train_loader=None, train_dataset=None, val_loader=None, val_dataset=None, label_smoothing=None,
-                 cuda=None, logger=None, attack=None, rand_target=None, attack_loss=None, scale_eps=None):
-        self.train_loader = train_loader
-        self.train_dataset = train_dataset
-        self.val_loader = val_loader
-        self.val_dataset = val_dataset
-        self.label_smoothing = label_smoothing
-        self.cuda = cuda
-        self.logger = logger
-        self.attack = attack
-        self.rand_target = rand_target
-        self.attack_loss = attack_loss
-        self.scale_eps = scale_eps
+    def __init__(self, **kwargs):
+
+        self.train_loader = kwargs['train_loader']
+        self.train_dataset = kwargs['train_dataset']
+        self.val_loader = kwargs['val_loader']
+        self.val_dataset = kwargs['val_dataset']
+
+        self.mode = kwargs['mode']
+        self.cuda = kwargs['cuda']
+        self.logger = kwargs['logger']
+        self.attack = kwargs['attack']
+        self.label_smoothing = 0
 
     def _compute_loss(self, output, target):
         if self.label_smoothing > 0:
@@ -95,21 +128,37 @@ class TrainerAdversCutout(BaseTrainer):
                 self._adjust_learning_rate(epoch)
                 loss = torch.zeros([], dtype=torch.float32, device='cuda')
 
-                # Compute clean accuracy & loss:
-                model.eval()
-                output = model(data)
-                train_std_loss_val = self._compute_loss(output, target)
-                train_std_loss.update(train_std_loss_val)
-                train_std_acc.update(accuracy(output, target))
-                model.train()
+                if self.mode == 'normal':
+                    # compute adversarial accuracy & loss:
+                    model.eval()
+                    output_adv = model(data_adv)
+                    adv_loss = self._compute_loss(output_adv, target)
+                    train_adv_loss.update(adv_loss)
+                    train_adv_acc.update(accuracy(output_adv, target))
 
-                output_adv = model(data_adv)
-                adv_loss = self._compute_loss(output_adv, target)
+                    # compute clean accuracy & loss (train mode):
+                    model.train()
+                    output = model(data)
+                    std_loss = self._compute_loss(output, target)
+                    train_std_loss.update(std_loss)
+                    train_std_acc.update(accuracy(output, target))
+                    loss += std_loss  # update using clean loss.
 
-                # Compute adversarial accuracy & loss:
-                train_adv_loss.update(adv_loss)
-                train_adv_acc.update(accuracy(output_adv, target))
-                loss += adv_loss
+                else:
+                    # compute clean accuracy & loss (eval mode):
+                    model.eval()
+                    output = model(data)
+                    train_std_loss_val = self._compute_loss(output, target)
+                    train_std_loss.update(train_std_loss_val)
+                    train_std_acc.update(accuracy(output, target))
+
+                    # compute adversarial accuracy & loss (train mode):
+                    model.train()
+                    output_adv = model(data_adv)
+                    adv_loss = self._compute_loss(output_adv, target)
+                    train_adv_loss.update(adv_loss)
+                    train_adv_acc.update(accuracy(output_adv, target))
+                    loss += adv_loss
 
                 optimizer.zero_grad()
                 loss.backward()
@@ -180,45 +229,35 @@ class TrainerAdversCutout(BaseTrainer):
 
 
 class BaseExperiment(object):
-    # Notes:
-    # The attack needs to be initialized after the cuda device is set
-    def __init__(self, batch_size=32, base_lr=0.0125, momentum=0.9, wd=1e-4, epochs=90, warmup_epochs=5, stride=10,
-                 label_smoothing=-1.0, rand_target=False, run_val=True, model=None, checkpoint_dir=None,
-                 dataset_path='/mnt/imagenet-test/', dataset=None, pre_augment=None, attack=None,
-                 attack_loss='avg', scale_eps=False, rand_init=True, logger=None, preprocess_options=None):
 
-        # Training options:
-        self.batch_size = batch_size
-        self.base_lr = base_lr
-        self.momentum = momentum
-        self.wd = wd
-        self.epochs = epochs
-        self.warmup_epochs = warmup_epochs
-        self.stride = stride
-        self.label_smoothing = label_smoothing
-        self.rand_target = rand_target
+    def __init__(self, model, **kwargs):
 
-        # Validation options:
-        self.run_val = run_val
+        # training options:
+        self.run_val = True
+        self.mode = kwargs['mode']
+
+        # optimization options
+        self.batch_size = kwargs['batch_size']
+        self.base_lr = kwargs['base_lr']
+        self.momentum = kwargs['momentum']
+        self.wd = kwargs['wd']
+        self.epochs = kwargs['epochs']
 
         # Model/checkpoints options
         self.model = model
-        self.checkpoint_dir = checkpoint_dir
-        self.dataset_path = dataset_path
+        self.checkpoint_dir = kwargs['checkpoint_dir']
+        self.dataset_path = kwargs['dataset_path']
 
-        # Attack options
-        self.attack = attack
-        self.attack_loss = attack_loss
-        self.scale_eps = scale_eps
-        self.rand_init = rand_init
+        # attack options:
+        self.attack = kwargs['attack']
+        self.normal_aug = kwargs['normal_aug']
+        self.advers_aug = kwargs['advers_aug']
 
-        # Logging options
-        self.logger = logger
+        # logging options:
+        self.logger = kwargs['logger']
 
-        # Data options:
-        self.dataset = dataset
-        self.pre_augment = pre_augment
-        self.preprocess_options = preprocess_options
+        # dataset options:
+        self.dataset = kwargs['dataset']
 
         # Dataset instance args. Set by subclass e.g. CIFAR10Trainer
         self.train_loader = None
@@ -228,12 +267,11 @@ class BaseExperiment(object):
         self.trainer = None
         self.optimizer = None
 
-        assert self.attack_loss in ['avg', 'adv_only', 'logsumexp', 'max']
         assert self.model is not None
 
-        # Set up checkpointing
-        if self.checkpoint_dir is not None:
-            os.makedirs(self.checkpoint_dir, exist_ok=True)
+        # set up checkpointing:
+        self.checkpoint_dir = os.path.join(self.logger.log_dir, 'checkpoints')
+        os.makedirs(self.checkpoint_dir, exist_ok=True)
 
         self.cuda = True
         self.verbose = 1
@@ -246,7 +284,7 @@ class BaseExperiment(object):
 
         assert self.attack, 'attack is {}'.format(self.attack)
 
-        # Note: trainer must be initialized after initializing the loaders.
+        # note: trainer must be initialized after initializing the loaders.
         self._init_loaders()
         self._init_optimizer()
         self._init_trainer()
@@ -259,11 +297,7 @@ class BaseExperiment(object):
                                    weight_decay=self.wd)
 
     def _init_trainer(self):
-        self.trainer = TrainerAdversCutout(train_loader=self.train_loader, train_dataset=self.train_dataset,
-                                           val_loader=self.val_loader, val_dataset=self.val_dataset,
-                                           label_smoothing=self.label_smoothing, cuda=self.cuda, attack=self.attack,
-                                           rand_target=self.rand_target, attack_loss=self.attack_loss,
-                                           scale_eps=self.scale_eps)
+        raise NotImplementedError
 
     def _checkpoint(self, epoch):
         if self.checkpoint_dir:
@@ -279,17 +313,20 @@ class BaseExperiment(object):
         for epoch in range(self.epochs):
             begin = time.time()
             self.model, self.optimizer, train_log_dict = self.trainer.train_epoch(epoch, self.model, self.optimizer)
-            self.logger.add(train_log_dict, epoch)
+            self.logger.add(train_log_dict)
 
             if self.run_val:
                 self.model, self.optimizer, val_log_dict = \
                     self.trainer.val_epoch(epoch, self.model, self.optimizer, self.verbose)
-                self.logger.add(val_log_dict, epoch)
+                self.logger.add(val_log_dict)
 
             self.logger.write_vals()
-            self.logger.write_plot(['train_std_acc', 'val_std_acc'], description='Train vs. Valid Accuracy')
-            self.logger.write_plot(['train_adv_acc', 'val_adv_acc'], description='Adversarial Train vs. Valid Accuracy')
-            self.logger.write_plot(['val_adv_acc', 'val_std_acc'], description='Adversarial Valid vs. Clean Accuracy')
+            self.logger.write_plot('std_train_vs_std_valid_acc', ['train_std_acc', 'val_std_acc'],
+                                   description='Train vs. Valid Accuracy')
+            self.logger.write_plot('train_adv_vs_val_adv_acc', ['train_adv_acc', 'val_adv_acc'],
+                                   description='Adversarial Train vs. Valid Accuracy')
+            self.logger.write_plot('val_adv_vs_val_std_acc', ['val_adv_acc', 'val_std_acc'],
+                                   description='Adversarial Valid vs. Clean Accuracy')
 
             self._checkpoint(epoch)
             end = time.time()
@@ -299,47 +336,9 @@ class BaseExperiment(object):
         self.logger.save_ckpt(self.model, self.optimizer)
 
 
-class CIFAR10Advers(datasets.CIFAR10):
-    def __init__(self, transform_adv=None, **kwargs):
-        super(CIFAR10Advers, self).__init__(**kwargs)
-        self.transform_adv = transform_adv
-
-    def __getitem__(self, index):
-        """
-        Args:
-            index (int): Index
-
-        Returns:
-            tuple: (image, target) where target is index of the target class.
-        """
-
-        img, target = self.data[index], self.targets[index]
-
-        # doing this so that it is consistent with all other datasets
-        # to return a PIL Image
-        img = Image.fromarray(img)
-        img_adv = img
-
-        if self.transform is not None:
-            img = self.transform(img)
-
-        if self.transform_adv is not None:
-            img_adv = self.transform_adv(img_adv)
-
-        if self.target_transform is not None:
-            target = self.target_transform(target)
-
-        output = (img, img_adv, target)
-        return output
-
-
 class CIFAR10Experiment(BaseExperiment):
     def __init__(self, **kwargs):
-        if 'epochs' not in kwargs:
-            kwargs['epochs'] = 200
-        if 'base_lr' not in kwargs:
-            kwargs['base_lr'] = 0.1
-        super().__init__(**kwargs)
+        super(CIFAR10Experiment, self).__init__(**kwargs)
 
     def _adjust_learning_rate(self, epoch):
         if epoch < 100:
@@ -351,41 +350,60 @@ class CIFAR10Experiment(BaseExperiment):
         for param_group in self.optimizer.param_groups:
             param_group['lr'] = lr
 
-    def _init_loaders(self):
+    def _get_transform(self, transform_options):
         normalize = transforms.Normalize(mean=[x / 255.0 for x in [125.3, 123.0, 113.9]],
                                          std=[x / 255.0 for x in [63.0, 62.1, 66.7]])
-        train_transform = transforms.Compose([])
-        is_tensor = False
 
-        for option in self.preprocess_options.split('-'):  # e.g. [standard, normalize].
+        transform = transforms.Compose([])
+        is_tensor = False
+        for option in transform_options:  # e.g. [standard, normalize].
             if option == 'standard':
                 if is_tensor:
-                    train_transform.transforms.append(transforms.ToPILImage())
+                    transform.transforms.append(transforms.ToPILImage())
                     is_tensor = False
-                train_transform.transforms.append(transforms.RandomCrop(32, padding=4))
-                train_transform.transforms.append(transforms.RandomHorizontalFlip())
+                transform.transforms.append(transforms.RandomCrop(32, padding=4))
+                transform.transforms.append(transforms.RandomHorizontalFlip())
 
             if option == 'advers':
                 if not is_tensor:
-                    train_transform.transforms.append(transforms.ToTensor())
+                    transform.transforms.append(transforms.ToTensor())
                     is_tensor = True
-                train_transform.transforms.append(self.attack)
+                transform.transforms.append(self.attack)
 
             if option == 'normalize':
                 if not is_tensor:
-                    train_transform.transforms.append(transforms.ToTensor())
+                    transform.transforms.append(transforms.ToTensor())
                     is_tensor = True
-                train_transform.transforms.append(normalize)
+                transform.transforms.append(normalize)
 
         if not is_tensor:
-            train_transform.transforms.append(transforms.ToTensor())
+            transform.transforms.append(transforms.ToTensor())
 
-        val_transform = transforms.Compose([transforms.ToTensor(), normalize])
+        return transform
 
-        self.train_dataset = CIFAR10Advers(root=self.dataset_path, download=True, train=True,
-                                           transform=val_transform, transform_adv=train_transform)
-        self.val_dataset = CIFAR10Advers(root=self.dataset_path, train=False, transform=val_transform,
-                                         transform_adv=train_transform)
+    def _init_loaders(self):
+        normalize = transforms.Normalize(mean=[x / 255.0 for x in [125.3, 123.0, 113.9]],
+                                         std=[x / 255.0 for x in [63.0, 62.1, 66.7]])
+
+        normal_transform = self._get_transform(self.normal_aug.split('-'))
+        advers_transform = self._get_transform(self.advers_aug.split('-'))
+        clean_transform = transforms.Compose([transforms.ToTensor(), normalize])
+
+        if self.mode == 'advers':
+            self.train_dataset = DatasetAdversCIFAR10(root=self.dataset_path, download=True, train=True,
+                                                      transform=clean_transform, transform_adv=advers_transform)
+            # the data transformed with <advers_transform> is used to train model with.
+            # the data transformed with <clean_transform> is used to evaluate the training loss with.
+
+        else:
+            self.train_dataset = DatasetAdversCIFAR10(root=self.dataset_path, download=True, train=True,
+                                                      transform=normal_transform, transform_adv=advers_transform)
+
+            # data with <normal_transform> is used to train model + test clean train loss.
+            # data with <advers_transform> is used to test advers train loss.
+
+        self.val_dataset = DatasetAdversCIFAR10(root=self.dataset_path, train=False, transform=clean_transform,
+                                                transform_adv=advers_transform)
 
         self.train_loader = torch.utils.data.DataLoader(
                 self.train_dataset, batch_size=self.batch_size,
@@ -394,3 +412,16 @@ class CIFAR10Experiment(BaseExperiment):
         self.val_loader = torch.utils.data.DataLoader(
                 self.val_dataset, batch_size=self.batch_size,
                 shuffle=False, pin_memory=True)
+
+    def _init_trainer(self):
+        kwargs = {
+            'train_loader': self.train_loader,
+            'train_dataset': self.train_dataset,
+            'val_loader': self.val_loader,
+            'val_dataset': self.val_dataset,
+            'cuda': self.cuda,
+            'logger': self.logger,
+            'attack': self.attack,
+            'mode': self.mode}
+
+        self.trainer = TrainerAdversCutout(**kwargs)
